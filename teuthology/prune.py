48f5d8c31a51a1132e7b58ebf93f75de5fa2b5f6
Zack Cerza <zack@redhat.com>
2015-07-30 10:14:34 -0600

    f

--- teuthology/prune.py
+++ teuthology/prune.py
@@ -48,7 +48,7 @@ def prune_archive(archive_dir, pass_days, remotes_days, dry_run=False):
 
 
 def listdir(path):
-    with safe_while(sleep=1, increment=1, tries=3) as proceed:
+    with safe_while(sleep=3, increment=3, tries=3) as proceed:
         while proceed():
             try:
                 return os.listdir(path)
